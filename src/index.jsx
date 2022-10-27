a70b6d95f5d88d99890c39ee5b57a66ff4133ccf
shukina7olga <olga.shchukina.99@mail.ru>
2022-10-27 23:45:33 +0300

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {createRoot} from 'react-dom/client';
+import './index.scss';
+
+import Module from "./module";
+
+
+const root = createRoot(document.getElementById('root'));
+
+root.render(<Module/>);
---
+++ src/module.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import kit from './img/IMG_20210219_120356.jpg'
+
+const Module = () => (
+    <>
+        <h1>Kiti koti кошки</h1>
+        <img width={200} src={kit}/>
+    </>
+);
+
+export default Module;
